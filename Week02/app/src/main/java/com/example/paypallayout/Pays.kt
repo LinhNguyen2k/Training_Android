2295287df0c9e6e13a7f0e5c1f2bd311ebd6c954
LinhNguyen2k <nguyenanhlinh27092000@gmail.com>
2021-08-11 17:54:31 +0700

    update day02

---
+++ Week02/app/src/main/java/com/example/paypallayout/Pays.kt
@@ -0,0 +1,11 @@
+package com.example.paypallayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pays : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pays)
+    }
+}
