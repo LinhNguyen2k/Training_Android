351eb9089f7a4b0426d5dc8fae981915bd549e6a
LinhNguyen2k <nguyenanhlinh27092000@gmail.com>
2021-08-12 14:11:57 +0700

    update layout cards

--- Week02/app/src/main/java/com/example/paypallayout/Home.kt
+++ Week02/app/src/main/java/com/example/paypallayout/Home.kt
@@ -1,11 +1,19 @@
 package com.example.paypallayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        bottom_NVT.background = null
+        layout_info.setOnClickListener {
+            val intent: Intent = Intent(applicationContext,Pays::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- Week02/app/src/main/java/com/example/paypallayout/Pays.kt
+++ Week02/app/src/main/java/com/example/paypallayout/Pays.kt
@@ -1,11 +1,21 @@
 package com.example.paypallayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_pays.*
 
 class Pays : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pays)
+        img_back.setOnClickListener {
+            finish()
+        }
+        btn_enviar.setOnClickListener {
+            val intent: Intent = Intent(applicationContext,Cards::class.java)
+            startActivity(intent)
+        }
     }
 }
