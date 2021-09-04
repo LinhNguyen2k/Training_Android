1e7282d1adbacc1a4150ab11a3da8d86f43b0d3c
LinhNguyen2k <nguyenanhlinh27092000@gmail.com>
2021-09-04 17:04:31 +0700

    done

--- Week03/app/src/main/java/com/example/week03/Home_Activity.kt
+++ Week03/app/src/main/java/com/example/week03/Home_Activity.kt
@@ -9,50 +9,48 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.Navigator
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home_Activity : AppCompatActivity() {
-    val fragment: FragmentManager = supportFragmentManager
+
+    lateinit var navHost : NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         supportActionBar!!.hide()
-         var navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
-         var controller : NavController = navHost.navController
+        navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        var controller: NavController = navHost.navController
         bottom_NVT.setupWithNavController(controller)
 
 
     }
 
     override fun onBackPressed() {
-
-        val fragmentTransaction: FragmentTransaction = fragment.beginTransaction()
-
-
-        val count = supportFragmentManager.backStackEntryCount
-        if (count == 0){
+        if(navHost.childFragmentManager.backStackEntryCount == 0){
+//        val count = supportFragmentManager.backStackEntryCount
+//        if (count == 0) {
             val eBundle = AlertDialog.Builder(this)
             eBundle.setTitle("Exit")
             eBundle.setIcon(R.drawable.ic_baseline_warning_24)
             eBundle.setMessage("Are you sure you wan exit app ?")
-            eBundle.setPositiveButton("Yes"){
-                    Dialog, which ->
-                finish()
+            eBundle.setPositiveButton("Yes") { Dialog, which ->
+                super.onBackPressed()
 
             }
-            eBundle.setNegativeButton("No"){
-                    Dialog, which ->
-                Toast.makeText(this,"",Toast.LENGTH_SHORT)
+            eBundle.setNegativeButton("No") { Dialog, which ->
+                Toast.makeText(this, "", Toast.LENGTH_SHORT)
             }
             val createBuild = eBundle.create()
             createBuild.show()
-        }else {
-            supportFragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
         }
 
 
-//        super.onBackPressed()
+
     }
 }
